ff49eacb53e43396f5af54842d8a1cdd492e8f41
EamonnWalsh01 <123598982+EamonnWalsh01@users.noreply.github.com>
2024-02-13 09:31:39 +0000

    fixed imports

--- scraping_data.py
+++ scraping_data.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, exc
+from sqlalchemy import exc 
 import requests
 import datetime
 
