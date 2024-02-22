2f3ae9882cc52e919f6d086b87bd1cdc2d95f3fc
Barry <barry@upcmail.ie>
2024-02-22 09:40:09 +0000

    flask port 80

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def root():
     return app.send_static_file('index.html')
 
 if __name__=="__main__":
-    app.run(debug=True, host="0.0.0.0",port=8080)
+    app.run(debug=True, host="0.0.0.0",port=80)
