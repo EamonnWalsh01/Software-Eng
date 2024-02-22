31b5985864e42435d6fe5798b07f107ba362a7fc
Barry <barry@upcmail.ie>
2024-02-22 09:59:39 +0000

    sfd

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def root():
-    return "Hello World"
+    return app.send_static_file('index.html')
 
 if __name__=="__main__":
     app.run(debug=True, host="0.0.0.0",port=8080)
