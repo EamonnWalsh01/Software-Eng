43308a8b2aa594aa03ed7eaf89d874a3c3f58efe
charliedrumm <charlie.drumm@ucdconnect.ie>
2024-02-22 10:29:44 +0000

    stations

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
+import sqlalchemy
+import configparser
 
 app = Flask(__name__, static_url_path='')
+
+config = configparser.ConfigParser()
+config.read('configbc.ini')
+db_config = config['database']
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{db_config['username']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['dbname']}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = sqlalchemy(app)
+
+@app.route('/stations')
+def get_stations():
+    sql = 'SELECT * FROM stations'
+    result = db.engine.execute(sql)
+    stations = [dict(row) for row in result]
+    return jsonify(stations)
+
 
 @app.route('/')
 def root():
