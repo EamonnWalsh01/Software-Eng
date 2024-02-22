f9e4a21ca2798dec72bca569d8b32150da1e99f0
Barry <barry@upcmail.ie>
2024-02-22 09:34:04 +0000

    flask test

--- weather.py
+++ weather.py
@@ -18,32 +18,6 @@ api_config=config['weather_api']
 # # Your API key 
 api_key = 'ca224502e7a2a285e6b4bf2e73ddf465'
 
-# # Location for which you want the weather data (e.g., "London,uk")
-# longitude = 53.35179289261352
-# latitude =-6.261408882271609
-
-# # Build the URL for the API request using coordinates
-# url = f'http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}'
-
-# # Make the GET request to the OpenWeatherMap API
-# response = requests.get(url)
-
-# # Check if the request was successful
-# if response.status_code == 200:
-#     # Parse the JSON response
-#     data = response.json()
-#     with open('weather_data.json', 'w') as file:
-#         json.dump(data, file, indent=4)
-#     # Extract the information you're interested in
-#     # Example: get the temperature, humidity, and weather description
-#     temperature = data['main']['temp']
-#     humidity = data['main']['humidity']
-#     weather_description = data['weather'][0]['description']
-    
-    
-# else:
-#     print("Failed to retrieve data")
-
 class Weather(Base):
     __tablename__ = 'weather'
     lon = Column(Float, primary_key=True, nullable=False)
