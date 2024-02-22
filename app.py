22bf2316de54f3e0913652b0eb636ba6d5d91b0e
charliedrumm <charlie.drumm@ucdconnect.ie>
2024-02-22 10:42:45 +0000

    statoin

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ DATABASE_URI =  f"mysql+pymysql://{db_config['username']}:{db_config['password']
 
 engine = create_engine(DATABASE_URI)
 
-@app.route('/stations')
+@app.route('/station')
 def get_stations():
     with engine.connect() as connection:
-        result = connection.execute(text("SELECT * FROM stations"))
+        result = connection.execute(text("SELECT * FROM station"))
         stations = [dict(row) for row in result]
     return jsonify(stations)
 
