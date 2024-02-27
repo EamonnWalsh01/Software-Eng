eaf57a5e98bc191fd6b985ed3096f527c0939fa5
Barry <barry@upcmail.ie>
2024-02-27 09:46:14 +0000

    wetaher

--- app.py
+++ app.py
@@ -79,6 +79,35 @@ def nearest_stations():
     return jsonify(nearest_stations)
 
 
+@app.route('/weather')
+def current_weather():
+    lat = request.args.get('lat', type=float)
+    lng = request.args.get('lng', type=float)
+
+    with engine.connect as connection:
+        query=text("""
+            SELECT lon, lat, temp, feels_like, humidity, rain_1h, weather_desc, weather_brief, wind_speed,
+                   SQRT(POW(69.1 * (s.position_lat - :lat), 2) +
+                    POW(69.1 * (:lng - s.position_lng) * COS(s.position_lat / 57.3), 2)) AS distance
+            FROM weather 
+            ORDER BY datetime, distance DESC 
+            LIMIT 1;      
+                """)
+        
+        params = {'lat': lat, 'lng': lng}  # Define parameters as a dictionary
+
+        with engine.connect() as connection:
+            result = connection.execute(query, params)  # Pass parameters dictionary as the second argument
+            nearest_weather = [dict(row) for row in result.mappings()]
+            print(nearest_weather)
+        return jsonify(nearest_weather)
+        
+    
+   
+
+    
+
+
 @app.route('/')
 def index():
     return app.send_static_file('map2.html')
