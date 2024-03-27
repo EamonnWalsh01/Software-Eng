4b19eaf05673af442927c91c1c861c3755162f8e
EamonnWalsh01 <123598982+EamonnWalsh01@users.noreply.github.com>
2024-03-27 22:33:17 +0000

    modeling

--- fakeBrain.py
+++ fakeBrain.py
@@ -3,31 +3,37 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
 import pandas as pd
 df = pd.read_csv('stations_data.csv', sep=',')
+def map_to_five(x):
+    return min(x, 5)
 
-y_set = pd.DataFrame(df['available_bike_stands']>0)
+# Applying the function to the 'available_bike_stands' column
+y_set = pd.DataFrame(df['available_bike_stands'].apply(map_to_five))
 
 x_set =df.select_dtypes(include=['number'])
 print(f"x_set shape: {x_set.shape}")
 #x_set = df.drop(['available_bike_stands',"name","weather_desc","weather_brief",], axis=1)
-train_x = x_set.sample(frac=.5, replace=True)
+train_x = x_set.sample(frac=.9, replace=True)
 
-remainder_df = df.drop(train_x.index)
 
 # For the remainder dataset, separate X and Y as you did initially
 remainder_df = df.drop(train_x.index)
 remainder_x = remainder_df.select_dtypes(include=['number'])
-remainder_y = remainder_df['available_bike_stands']>0
+remainder_y = remainder_df['available_bike_stands'].apply(map_to_five)
 
 y_train = y_set.loc[train_x.index]
 
 
 model = tf.keras.Sequential([
       tf.keras.layers.Normalization(input_shape=[11,], axis=None),
-      # tf.keras.layers.Dense(8, activation='relu'),
-       tf.keras.layers.Dense(7, activation='relu'),
-      # tf.keras.layers.Dense(6, activation='relu'),
-      tf.keras.layers.Dense(5, activation='relu'),
-      tf.keras.layers.Dense(5)
+       tf.keras.layers.Dense(100, activation='relu'),
+       #tf.keras.layers.Dense(1000, activation='relu'),
+       #tf.keras.layers.Dense(1000, activation='relu'),
+      #tf.keras.layers.Dense(1000, activation='relu'),
+       #     tf.keras.layers.Dense(1000, activation='relu'),
+      tf.keras.layers.Dense(100, activation='relu'),
+      tf.keras.layers.Dense(100, activation='relu'),
+
+      tf.keras.layers.Dense(6,use_bias=True)
   ])
 # Add the hidden layers
 
@@ -36,7 +42,7 @@ model = tf.keras.Sequential([
 
 # Compile the model
 model.compile(optimizer='adam', loss='mean_squared_error',metrics=['accuracy'])
-model.fit(train_x, y_train, epochs=2)
+model.fit(train_x, y_train, epochs=20)
 print("hi")
 evaluation = model.evaluate(remainder_x, remainder_y, verbose=2)
 print(f"Model evaluation on remainder set: {evaluation}")
